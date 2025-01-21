0b8b1c579acbbdbb82ac75c9305cb89a9f0bf1af
jeremycho1999 <118226430+jeremycho1999@users.noreply.github.com>
2025-01-20 20:56:39 -0500

    changed win text

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -845,7 +845,7 @@ Try to beat us ➡️ https://wordsynced.com?utm_source=share_score&utm_medium=t
             <Confetti />
             <div>
               <h2 className="text-2xl font-bold text-gray-900 mb-2">
-                You Got Synced!
+                You Got Synced! 🔄
               </h2>
               <p className="text-lg text-gray-600">
                 It took {currentRound} rounds to get synced!
